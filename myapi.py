20c86a2c3ce94845fd09914b6f7017c516606ca6
mahesh927 <2211cs010631@mallareddyuniversity.ac.in>
2025-02-09 21:50:29 +0530

    this is fastapi file

---
+++ myapi.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+@app:get("/")
+def read_root():
+    return {"Hello": "World"}   
+@app:get('/about')
+def read_about():
+    return {"about": "This is a about page"}
